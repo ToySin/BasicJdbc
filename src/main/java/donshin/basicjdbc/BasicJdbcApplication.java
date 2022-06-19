60b9958175673c3cc94ee2e4bd463dffd6f28822
Toysin <toysin1203@hotmail.com>
2022-06-19 14:52:37 +0900

    초기화

---
+++ src/main/java/donshin/basicjdbc/BasicJdbcApplication.java
@@ -0,0 +1,13 @@
+package donshin.basicjdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicJdbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BasicJdbcApplication.class, args);
+    }
+
+}
---
+++ src/test/java/donshin/basicjdbc/BasicJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package donshin.basicjdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasicJdbcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
