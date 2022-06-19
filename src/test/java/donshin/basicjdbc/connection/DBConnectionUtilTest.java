debf12ad8f874e61a4ecd3214b50ee916fc6786c
Toysin <toysin1203@hotmail.com>
2022-06-19 23:29:29 +0900

    DriverManager를 통한 커넥션 획득 테스트 -> 시도마다 새로운 커넥션

--- src/test/java/donshin/basicjdbc/connection/DBConnectionUtilTest.java
+++ src/test/java/donshin/basicjdbc/connection/DBConnectionUtilTest.java
@@ -1,13 +1,14 @@
 package donshin.basicjdbc.connection;
 
 import lombok.extern.slf4j.Slf4j;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.jupiter.api.Assertions.*;
+import static donshin.basicjdbc.connection.ConnectionConst.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @Slf4j
 class DBConnectionUtilTest {
@@ -16,5 +17,13 @@ class DBConnectionUtilTest {
     void connection() {
         Connection con = DBConnectionUtil.getConnection();
         assertThat(con).isNotNull();
+    }
+
+    @Test
+    void driverManager() throws SQLException {
+        Connection con1 = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        Connection con2 = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        log.info("connection={}, class={}", con1, con1.getClass());
+        log.info("connection={}, class={}", con2, con2.getClass());
     }
 }
