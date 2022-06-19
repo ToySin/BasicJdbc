de43cdf961b8d6afde781ecf29c57c78a32ef698
Toysin <toysin1203@hotmail.com>
2022-06-19 23:36:36 +0900

    DriverManagerDriverSource로 커넥션 획득 -> 매번 새로운 커넥션 생성은 동일하지만 dataSource를 생성해두었기 때문에 매 생성마다 URL, USERNAME, PASSWORD를 전달할 필요가 없다.

--- src/test/java/donshin/basicjdbc/connection/ConnectionTest.java
+++ src/test/java/donshin/basicjdbc/connection/ConnectionTest.java
@@ -2,6 +2,7 @@ package donshin.basicjdbc.connection;
 
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -16,6 +17,16 @@ public class ConnectionTest {
     void driverManager() throws SQLException {
         Connection con1 = DriverManager.getConnection(URL, USERNAME, PASSWORD);
         Connection con2 = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        log.info("connection={}, class={}", con1, con1.getClass());
+        log.info("connection={}, class={}", con2, con2.getClass());
+    }
+
+    @Test
+    void dataSourceDriverManager() throws SQLException {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource(URL, USERNAME, PASSWORD);
+
+        Connection con1 = dataSource.getConnection();
+        Connection con2 = dataSource.getConnection();
         log.info("connection={}, class={}", con1, con1.getClass());
         log.info("connection={}, class={}", con2, con2.getClass());
     }
