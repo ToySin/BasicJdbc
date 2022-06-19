6f3a0c0d7b3e1b3965455f6b90364b48087dd7a5
Toysin <toysin1203@hotmail.com>
2022-06-19 15:15:16 +0900

    Connection 상수 정의 (URL, USERNAME, PASSWORD)

--- src/main/java/donshin/basicjdbc/connection/ConnectionConst.java
+++ src/main/java/donshin/basicjdbc/connection/ConnectionConst.java
@@ -1,7 +1,7 @@
 package donshin.basicjdbc.connection;
 
 public class ConnectionConst {
-    public final String URL = "jdbc:h2:tcp://localhost/~/h2_db/2.1.212/jdbcstudy";
-    public final String USERNAME = "sa";
-    public final String PASSWORD = "";
+    public static final String URL = "jdbc:h2:tcp://localhost/~/h2_db/2.1.212/jdbcstudy";
+    public static final String USERNAME = "sa";
+    public static final String PASSWORD = "";
 }
