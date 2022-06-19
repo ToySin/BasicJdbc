03c7635fcf4293a375a4f5f636fde7aadadcbaaf
Toysin <toysin1203@hotmail.com>
2022-06-19 17:14:07 +0900

    MemberRepositoryV0 findById 기능 구현

--- src/main/java/donshin/basicjdbc/repository/MemberRepositoryV0.java
+++ src/main/java/donshin/basicjdbc/repository/MemberRepositoryV0.java
@@ -5,6 +5,7 @@ import donshin.basicjdbc.domain.Member;
 import lombok.extern.slf4j.Slf4j;
 
 import java.sql.*;
+import java.util.NoSuchElementException;
 
 @Slf4j
 public class MemberRepositoryV0 {
@@ -26,6 +27,35 @@ public class MemberRepositoryV0 {
             throw e;
         } finally {
             close(con, pstmt, null);
+        }
+    }
+
+    public Member findById(String memberId) throws SQLException {
+        String sql = "select * from member where member_id = ?";
+
+        Connection con = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try {
+            con = DBConnectionUtil.getConnection();
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, memberId);
+            rs = pstmt.executeQuery();
+
+            if (rs.next()) {
+                Member findMember = new Member();
+                findMember.setMemberId(rs.getString("member_id"));
+                findMember.setMoney(rs.getInt("money"));
+                return findMember;
+            } else {
+                throw new NoSuchElementException("member not found memberId=" + memberId);
+            }
+        } catch (SQLException e) {
+            log.error("find member fail", e);
+            throw e;
+        } finally {
+            close(con, pstmt, rs);
         }
     }
 
