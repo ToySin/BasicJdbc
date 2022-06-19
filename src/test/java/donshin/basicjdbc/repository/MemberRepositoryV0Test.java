1792c732e7c52a07fdb689fc8813bf11fd701905
Toysin <toysin1203@hotmail.com>
2022-06-19 18:00:50 +0900

    로그 추가

--- src/test/java/donshin/basicjdbc/repository/MemberRepositoryV0Test.java
+++ src/test/java/donshin/basicjdbc/repository/MemberRepositoryV0Test.java
@@ -1,23 +1,20 @@
 package donshin.basicjdbc.repository;
 
 import donshin.basicjdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.sql.SQLException;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+@Slf4j
 class MemberRepositoryV0Test {
 
     MemberRepositoryV0 memberRepositoryV0 = new MemberRepositoryV0();
 
     @AfterEach
     void afterEach() {
-        String sql = "delete from member";
-
 
     }
 
@@ -26,9 +23,11 @@ class MemberRepositoryV0Test {
         //save
         Member member = new Member("memberA", 10000);
         memberRepositoryV0.save(member);
+        log.info("member save={}", member);
 
         //find
         Member findMember = memberRepositoryV0.findById("memberA");
+        log.info("member find={}", findMember);
         Assertions.assertThat(findMember.getMemberId()).isEqualTo(member.getMemberId());
         Assertions.assertThat(findMember.getMoney()).isEqualTo(member.getMoney());
     }
